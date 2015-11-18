66d1b763c20e338f8016c68e994835b3cf6f6525
Victor Diniz <blek.net@gmail.com>
2015-11-18 12:30:10 -0200

    Adiciona biblioteca para processamento de linhas do assembly

---
+++ sb-expander/src/line_parser.c
@@ -0,0 +1,35 @@
+#include "line_parser.h"
+#include <string.h>
+
+static void parseLine(char *in, Line *out){
+    *out = (Line){NULL, NULL, NULL, NULL};
+
+    // Ignores comments by cutting the string off at the comment delimiter
+    char *comment_delim = strchr(in, ';');
+    if (comment_delim)
+        *comment_delim = '\0';
+
+    char *pch = strtok(in, " \t\n");
+    if (!pch)
+        return;
+
+    char *colon = strchr(pch, ':');
+    if (colon){
+        *colon = '\0';
+        out->label = pch;
+        pch = strtok(NULL, " \t\n");
+        if (!pch)
+            return;
+    }
+
+    out->instr = pch;
+    pch = strtok(NULL, " \t\n");
+    if (!pch)
+        return;
+    out->op1 = pch;
+
+    pch = strtok(NULL, " \t\n");
+    if (!pch)
+        return;
+    out->op2 = pch;
+}
---
+++ sb-expander/src/line_parser.h
@@ -0,0 +1,20 @@
+#ifndef LINE_PARSER_H
+#define LINE_PARSER_H
+
+typedef struct {
+    char *label;
+    char *instr;
+    char *op1;
+    char *op2;
+} Line;
+
+/**
+ * Parses a line of assembly code, splitting it in its components: label, ope-
+ * rator and operands. Does NOT keep the original string intact.
+ * @param in  Line of assembly code.
+ * @param out Address to a Line struct, to which the line's components will be
+ *            saved.
+ */
+static void parseLine(char *in, Line *out);
+
+#endif
